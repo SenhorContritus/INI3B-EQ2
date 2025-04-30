8bb685e118a86f5fd136eb2cdffeed65675aa195
SenhorContritus <eduardo.martinez@unesp.br>
2025-04-30 12:15:44 -0300

    classe alarm finalizada

--- src/tccApp/Classes/Alarm.tsx
+++ src/tccApp/Classes/Alarm.tsx
@@ -2,11 +2,42 @@ import _address from "../types/_address"
 import AlarmProps from "./AlarmProps";
 
 class Alarm{
-    private id: number;
-    private name: string;
-    private address: _address;
-    private alarmProps: AlarmProps;
+    private _id: number;
+    private _name: string;
+    private _address: _address;
+    private _alarmProps: AlarmProps;
     
+    public constructor(id: number, name: string, address: _address, alarmProps: AlarmProps){
+        this._id = id;
+        this._name = name;
+        this._address = address;
+        this._alarmProps = alarmProps;
+    }
+
+    get id(){
+        return this._id;
+    }
+    set id(id:number){
+        this._id = id
+    }
+    get name(){
+        return this._name;
+    }
+    set name(name:string){
+        this.name = name
+    }
+    get address(){
+        return this._address;
+    }
+    set address(address:_address){
+        this._address = address
+    }
+    get alarmProps(){
+        return this._alarmProps;
+    }
+    set alarmProps(alarmProps: AlarmProps){
+        this._alarmProps = alarmProps
+    }
 
 }
 
--- src/tccApp/Classes/AlarmProps.tsx
+++ src/tccApp/Classes/AlarmProps.tsx
@@ -2,7 +2,7 @@ import _prostponeProps from "../types/_prostponeProps";
 import Alarm from "./Alarm";
 
 class AlarmProps{
-    private id: number;
+    private propsId: number;
     private startTime: number[];
     private sound: boolean;
     private soundUrl: string;
@@ -12,6 +12,17 @@ class AlarmProps{
     private prostponeProps: _prostponeProps;
     private volume: number;
 
+    public constructor(propsId: number, startTime:number[], sound: boolean, soundUrl:string, vibration:boolean, vibrationType:string, prostpone:boolean, prostponeProps: _prostponeProps, volume:number){
+        this.propsId = propsId;
+        this.startTime = startTime;
+        this.sound = sound;
+        this.soundUrl = soundUrl;
+        this.vibration = vibration;
+        this.vibrationType = vibrationType;
+        this.prostpone = prostpone;
+        this.prostponeProps = prostponeProps;
+        this.volume = volume;
+    }
 
 }
 export default AlarmProps
