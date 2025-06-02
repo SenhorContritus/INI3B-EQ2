850fb08b13b1f5ca1e7ba4a3574225d397cb521c
João Vitor Mendes Pascolat <jv.pascolat@unesp.br>
2025-06-02 11:50:12 -0300

    codigo corrijido com comentarios

--- src/tcc/Pages/Main.tsx
+++ src/tcc/Pages/Main.tsx
@@ -310,3 +310,30 @@ const styles = StyleSheet.create({
     fontSize: 15,
   },
 });
+/*
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+import Main from "./Pages/Main";
+import ConfigPessoal from "./Pages/ConfigPessoal";
+import ConfigurarAlarme from "./Pages/ConfigurarAlarme";
+import TocarAlarme from "./Pages/TocarAlarme";
+
+
+const Stack = createNativeStackNavigator();
+
+
+export default function App(){
+  return <NavigationContainer>
+    
+    <Stack.Navigator screenOptions={{headerShown:false}}>
+      <Stack.Screen name="Main" component={Main}/>
+      <Stack.Screen name="ConfigPessoal" component={ConfigPessoal} />
+      <Stack.Screen name="ConfigurarAlarme" component={ConfigurarAlarme} />
+      <Stack.Screen name="TocarAlarme" component={TocarAlarme} />
+    </Stack.Navigator>
+  </NavigationContainer>
+};
+
+*/ 
